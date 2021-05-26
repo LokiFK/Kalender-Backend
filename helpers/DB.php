ef6d7f905a557d2c92d5d0f0060a227472cb6766
David Ilchmann <david.ilchmann@gmail.com>
2021-05-26 08:15:18 +0200

    cutting down

--- helpers/DB.php
+++ helpers/DB.php
@@ -2,28 +2,27 @@
 
     class DB {
 
-        private $connection;
-        private $host = '127.0.0.1';
-        private $dbname = 'calendar';
-        private $username = 'root';
-        private $password = '';
+        const host = '127.0.0.1';
+        const dbname = 'calendar';
+        const username = 'root';
+        const password = '';
 
-        public function __construct()
+        private static function connect()
         {
             try {
-                $pdo = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbname . ';charset=utf8', $this->username, $this->password);
+                $pdo = new PDO('mysql:host=' . DB::host . ';dbname=' . DB::dbname . ';charset=utf8', DB::username, DB::password);
                 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $this->connection = $pdo;
+                return $pdo;
             } catch (PDOException $e) {
                 UI::error(500, 'Error connecting to DB: ' . $e->getMessage());
                 exit;
             }
         }
 
-        public function query($query, $params = array())
+        public static function query($query, $params = array())
         {
             try {
-                $stmt = $this->connection->prepare($query);
+                $stmt = DB::connect()->prepare($query);
                 $stmt->execute($params);
 
                 if (explode(' ', $query)[0] == 'SELECT') {
@@ -32,7 +31,7 @@
                         return $result;
                     }
                 } else if (explode(' ', $query)[0] == 'INSERT') {
-                    return $this->connection->lastInsertID();
+                    return DB::connect()->lastInsertID();
                 }
             } catch (PDOException $e) {
                 echo $query;
@@ -41,10 +40,15 @@
             }
         }
 
-        public function queryColumns($tableName)
+        public static function table($tableName)
+        {
+            
+        }
+
+        public static function queryColumns($tableName)
         {
             try {
-                $stmt = $this->connection->prepare("DESCRIBE $tableName");
+                $stmt = DB::connect()->prepare("DESCRIBE $tableName");
                 $stmt->execute();
                 return $stmt->fetchAll(PDO::FETCH_COLUMN);
             } catch (PDOException $e) {
@@ -52,9 +56,58 @@
                 exit;
             }
         }
+    }
 
-        public static function db()
+    class TableReturn {
+        public const ASC = 0;
+        public const DESC = 1;
+
+        private $name;
+        private $contents;
+
+        public function __construct($name)
         {
-            return new DB();
+            $this->name = $name;
+            $this->contents = DB::query("SELECT * FROM :tableName", array(':tableName' => $name));
+        }
+
+        public function orderBy($column = array(), $direction = 0)
+        {
+            
+        }
+
+        public function get($columns = array())
+        {
+            if (count($columns) == 0) { return $this->contents; }
+
+            $returnArr = array();
+            if (count($columns) == 1) {
+                for ($i=0; $i < count($this->contents); $i++) {
+                    if (isset($this->contents[$i][$columns[0]])) {
+                        $returnArr[$i][$columns[0]] = $this->contents[$i][$columns[0]];
+                    }
+                }
+                return $returnArr;
+            }
+
+            for ($i=0; $i < count($this->contents); $i++) { 
+                for ($j=0; $j < count($columns); $j++) {
+                    if (isset($this->contents[$i][$columns[$j]])) {
+                        $returnArr[$i][$columns[$j]] = $this->contents[$i][$columns[$j]];
+                    }
+                }
+            }
+            
+            return $returnArr;
+        }
+    }
+
+
+    class TableContents {
+        private $contents;
+
+        public function __construct($contents)
+        {
+            $this->contents = $contents;
         }
     }
--- helpers/index.php
+++ helpers/index.php
@@ -2,8 +2,6 @@
 
     require_once './helpers/DB.php';
     require_once './helpers/Routes.php';
-    require_once './helpers/Queryable.php';
-    require_once './helpers/Model.php';
     require_once './helpers/UI.php';
     require_once './helpers/Request.php';
 
