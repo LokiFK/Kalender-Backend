3f20ac2b3290ec955b730be6ca599e1421ff9a0c
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2021-06-06 15:04:16 +0200

    new replaceFiles function

--- helpers/Response.php
+++ helpers/Response.php
@@ -26,7 +26,7 @@
             $pathCSS = "./public/css/$component.css";
             $pathDefaultCSS = "./public/css/default-styles.css";
             if (is_file($pathHTML)) {
-                $content = file_get_contents($pathHTML);
+                $content = replaceFiles( file_get_contents($pathHTML) );
                 foreach ($data as $key => $value) {
                     $content = str_replace('{{ ' . $key . ' }}', $value, $content);
                 }
@@ -42,4 +42,24 @@
             $res->error('Error setting up HTML');
             $res->errorCode(500);
         }
+        
+        public static function replaceFiles(string $component){
+            $i = 0;
+            while(true){
+               $j = strpos($component, "{{%", $i) + 3; 
+               if($j==false){
+                 return $component;  
+               } 
+               $k = strpos($component, "%}}", $j);
+               if($k==false){
+                 return $component;  
+               }    
+               $i = $k;
+               $path = substr($component, $j, $k-$j);
+               if(is_file($path)){
+                 $content = replaceFiles( file_getcontents($path) );
+                 $component = substr_replace($component, $content, $j, $k-$j);
+               }    
+            }   
+        }    
     }
