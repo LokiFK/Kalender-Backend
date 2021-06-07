8cdb9a9e9e7771ec2b7da3c37037100b98bc5aa0
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2021-06-07 20:21:30 +0200

    fixed replaceFile, Footer

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -3,19 +3,19 @@
     class UserController{
 
         public function appointmentMaking(Request $req, Response $res){
-          $view = $res->view('user/appointmentMaking');
+          $view = $res->view('user/appointmentMaking',["title"=>"Terminvereinbarung"]);
           $view = $res->view('user/template',['content'=>$view]);
           echo $view; 
         }
 
         public function appointmentOverview(Request $req, Response $res){
-            $view = $res->view('user/appointmentOverview');
+            $view = $res->view('user/appointmentOverview',["title"=>"Terminübersicht"]);
             $view = $res->view('user/template',['content'=>$view]);
             echo $view; 
         }
 
           public function profile(Request $req, Response $res){
-            $view = $res->view('user/profile');
+            $view = $res->view('user/profile',["title"=>"Profil"]);
             $view = $res->view('user/template',['content'=>$view]);
             echo $view; 
         }
