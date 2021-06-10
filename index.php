d8aba35068695afc9d08b81747d5c699dd960202
David Ilchmann <david.ilchmann@gmail.com>
2021-06-10 22:07:26 +0200

    updates

--- index.php
+++ index.php
@@ -34,17 +34,11 @@
     $routes->post('/auth/account/create', 'AuthController@createAccount');
 
     // Login
-<<<<<<< Updated upstream
-    $routes->get('/auth/login', 'AuthController@login');
-    $routes->get('/auth/logout', 'AuthController@logout');
-    $routes->get('/auth/getUser', 'AuthController@getUser');    //???
-=======
     $routes->post('/api/auth/login', 'AuthController@login');
 
     // Permissions
     $routes->get('/api/auth/permissions', 'AuthController@permissions');
     
->>>>>>> Stashed changes
 
     // --- ADMIN --- \\
 
