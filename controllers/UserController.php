21feb3d34246b3da96159813ed4249df1f870257
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2021-06-10 16:32:36 +0200

    some stuff

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -2,14 +2,13 @@
 
     class UserController {
 
-        public function appointmentMaking(Request $req, Response $res){
-            $view = $res->view('user/appointmentMaking', array(), array(), ['stuff' => ["1", "2", "3"], 'thing' => ["a", "b", "c"]]);
+        public function new(Request $req, Response $res){
+            $view = $res->view('user/new');
             echo $view;
-            DB::table('users')->where('username = :username', [':username' => 'david'])->get();
         }
 
-        public function appointmentOverview(Request $req, Response $res){
-            $view = $res->view('user/appointmentOverview');
+        public function overview(Request $req, Response $res){
+            $view = $res->view('user/overview');
             echo $view; 
         }
 
